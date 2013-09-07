176c680b5e18466b2ad0ba127e406e5f1ac36826
Ajay Aswal <aswal.ajay@hotmail.com>
2013-09-07 11:57:10 +0530

    Finish user CRUD actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+ 
   def new
     @user = User.new
   end
 
+  def edit
+  #  @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end 
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -25,4 +47,17 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
